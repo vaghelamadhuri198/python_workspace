db2cf5bf2827f1cc2c9ffd8c23f500bb50310102
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2025-12-27 11:24:10 +0530

    Variable.py
    Added a new Python program to declare and print variables.

---
+++ Variable.py
@@ -0,0 +1,13 @@
+# Variable Program in Python
+
+# Declaring variables
+name = "Meera"
+age = 25
+salary = 45000.50
+is_employee = True
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Salary:", salary)
+print("Is Employee:", is_employee)
